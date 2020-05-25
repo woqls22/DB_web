5ee87df4d8bba75086fdb43a4e1b30e6df54c948
LEEJAEBEEN <woqls226@gmail.com>
2020-05-25 19:22:04 +0900

    DB connect

--- server.js
+++ server.js
@@ -2,37 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const fs = require('fs');
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host:conf.host,
+    user: conf.user,
+    password:conf.password,
+    port:conf.port,
+    database:conf.database
+});
+connection.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/api/customers',(req,res)=>{
-    res.send([
-        {
-          'id':'1',
-          'image':'https://placeimg.com/64/64/any',
-          'name':'이재빈',
-          'birthday':'950810',
-          'gender':'남자',
-          'job':'대학생'
-        },
-        {
-          'id':'2',
-          'image':'https://placeimg.com/64/64/1',
-          'name':'이인석',
-          'birthday':'951010',
-          'gender':'남자',
-          'job':'대학생'
-        },
-        {
-          'id':'3',
-          'image':'https://placeimg.com/64/64/2',
-          'name':'이찬주',
-          'birthday':'950312',
-          'gender':'남자',
-          'job':'공돌이'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err,rows,fields)=>{
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port,()=>console.log('Listening on port '+port));
