8a66091e21bc241b04d6ab60444cb6559ee3b65d
LEEJAEBEEN <woqls226@gmail.com>
2020-05-25 18:29:43 +0900

    Api Loading view 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import Customer from './components/Customer'
 import './App.css';
-import { render } from '@testing-library/react';
 import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import {withStyles} from '@material-ui/core/styles'
-
+import {withStyles} from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 const styles = theme =>({
   root:{
     width : '100%',
@@ -18,21 +17,31 @@ const styles = theme =>({
   },
   table:{
     minWidth : 1080
+  },
+  progress:{
+    margin:theme.spacing.unit*2
   }
 })
 class App extends Component{
   state = {
-    customers:""
+    customers:"",
+    completed:0
   }
   componentDidMount(){
-    this.callApi().then(res=>this
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res=>this
       .setState({customers:res}))
-    .catch(err=>console.log(err));
+      .catch(err=>console.log(err));
   }
   callApi = async()=>{
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+  progress=()=>{
+    const {completed} = this.state;
+    this.setState({completed : completed>=100 ? 0 : completed+1})
   }
   render(){
     const {classes} = this.props;
@@ -51,17 +60,14 @@ class App extends Component{
           </TableHead>
           <TableBody>
           {this.state.customers ? this.state.customers.map(c=>{
-          return<Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-          />
-        }):""
-      }
+          return<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+        }):
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className = {classes.progress} variant = "determinate" value = {this.state.completed}/>
+            </TableCell>
+          </TableRow>
+        }
           </TableBody>
         </Table>
       </Paper>
